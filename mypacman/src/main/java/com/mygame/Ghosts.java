3c8208cf985e23992860899be82f98e8e3134eb6
Matthew L. <matthewdl0015@gmail.com>
2024-04-05 17:57:13 -0400

    Ghost now changes direction once wall is hit

--- mypacman/src/main/java/com/mygame/Ghosts.java
+++ mypacman/src/main/java/com/mygame/Ghosts.java
@@ -7,6 +7,7 @@ public class Ghosts {
     private float x_coord;
     private float y_coord;
     private String direction;
+    private String[] actions = {"North", "West", "South", "East"};
 
     public Ghosts() {
         direction = "East";
@@ -16,7 +17,17 @@ public class Ghosts {
 
     // Moves this ghost and updates location
     public void move() {
+        //Go until wall hit, then pick new direction
+
         float[] newPos = Agents.move(direction, new float[] {x_coord, y_coord});
+
+        // Check if a wall was hit
+        if(x_coord==newPos[0] && y_coord==newPos[1]) {
+            changeDirection();
+        }
+
+        //System.out.println("Before move: ("+x_coord+", "+y_coord+")");
+        //System.out.println("After move: ("+newPos[0]+", "+newPos[1]+")");
         x_coord = newPos[0];
         y_coord = newPos[1];
     }
@@ -24,5 +35,19 @@ public class Ghosts {
     // Returns this ghost's position as {x,y} array
     public float[] getPos() {
         return new float[] {x_coord, y_coord};
+    }
+
+    // Changes the direction if able
+    public void changeDirection() {
+        // Pick first action available
+        for(int i = 0; i < actions.length; i++) {
+            direction = actions[i];
+            float[] newPos = Agents.move(direction, new float[] {x_coord, y_coord});
+            if(x_coord!=newPos[0] || y_coord!=newPos[1]) {
+                break;
+            }
+            x_coord = newPos[0];
+            y_coord = newPos[1];
+        }
     }
 }
